a10bfa018032e98be5cf25321fd21934f1b19a10
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-09-14 22:52:31 +0300

    feat: make a markup for tablet

--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -7,7 +7,7 @@ function Techs() {
       <h2 className="techs__title">Технологии</h2>
       <h3 className="techs__sub-title">7 технологий</h3>
       <p className="techs__paragraph">
-        На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.
+        На&nbsp;курсе веб-разработки мы&nbsp;освоили технологии, которые применили в&nbsp;дипломном проекте.
       </p>
       <ul className="techs__list">
         <li><p className="techs__item">HTML</p></li>
