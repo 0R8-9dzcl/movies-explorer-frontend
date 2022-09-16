fbe8ad564a25c0eeaa9ad8cdafa2e8690d60d91a
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-09-16 19:01:05 +0300

    fix: delete empty tag

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,20 +3,18 @@ import { Link, Route } from 'react-router-dom';
 
 function Navigator() {
  return(
-  <>
-   <Route exact path='/' >
-      <nav className='nav'>
-        <ul className="nav__list">
-          <li>
-            <Link className="nav__link nav__link_type_land" to="/sign-up">Регистрация</Link>
-          </li>
-          <li>
-            <Link className="nav__link nav__link_type_land nav__link_type_login" to="/sign-in">Войти</Link>
-          </li>
-        </ul>
-      </nav>
-    </Route>
-  </>
+  <Route exact path='/' >
+    <nav className='nav'>
+      <ul className="nav__list">
+        <li>
+          <Link className="nav__link nav__link_type_land" to="/sign-up">Регистрация</Link>
+        </li>
+        <li>
+          <Link className="nav__link nav__link_type_land nav__link_type_login" to="/sign-in">Войти</Link>
+        </li>
+      </ul>
+    </nav>
+  </Route>
  )
 }
 
