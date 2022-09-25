d869e6158162bb6ad16bd8ee43078bc82ea3ddcf
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-09-25 14:15:19 +0300

    feat: add burger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,29 @@
 import './Header.css';
-import { Link, useLocation } from 'react-router-dom';
-import logo from '../../images/header-logo.svg';
+import { Link, useLocation, Route } from 'react-router-dom';
 import Navigator from '../Navigation/Navigation';
+import logo from '../../images/header__logo.svg';
+import burgeIcon from '../../images/header__burger.svg';
 
-function Header() {
+function Header({ onOpen, onClose, burgerOpen }) {
+  const userRoutes = ['/movies', '/saved-movies', '/profile'];
   const location = useLocation();
+  const openBurger = () => {
+    onOpen(true);
+  };
   return (
-    <header className={`header${
-      location.pathname === '/' ? ' header__theme_dark' : ''
-      }`}
+    <header
+      className={`header${location.pathname === '/' ? ' header_theme_dark' : ''}`}
     >
       <div className="header__container">
         <Link className="header__logo-button" to="/">
           <img className="header__logo" src={logo} alt="логотип сайта" />
         </Link>
-        <Navigator />
+        <Route exact path={userRoutes}>
+          <button type="button" className="header__burger" onClick={openBurger}>
+            <img src={burgeIcon} alt="кнопка откртия выпадающего меню" className="header__burger-icon" />
+          </button>
+        </Route>
+        <Navigator onClose={onClose} burgerOpen={burgerOpen} />
       </div>
     </header>
   );
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,21 +1,82 @@
+import {
+  Link,
+  NavLink,
+  Route, useLocation,
+} from 'react-router-dom';
 import './Navigation.css';
-import { Link, Route } from 'react-router-dom';
+import closedBurger from '../../images/nav__close-burger.svg';
 
-function Navigator() {
- return(
-  <Route exact path='/' >
-    <nav className='nav'>
+function Navigator({ onClose, burgerOpen }) {
+  const userRoutes = ['/movies', '/saved-movies', '/profile'];
+  const location = useLocation();
+  const closeBurger = () => {
+    onClose(false);
+  };
+  return (
+    <nav
+      className={
+        `nav${location.pathname === '/' ? '' : ' nav_type_logged'}${burgerOpen ? ' nav_type_burger-menu' : ''}`
+      }
+    >
+      <Route exact path={userRoutes}>
+        <button type="button" className="nav__close-burger" onClick={closeBurger}>
+          <img src={closedBurger} alt="кнопка закрытия выпадающего меню" className="nav__close-burger-icon" />
+        </button>
+        <ul className="nav__films">
+          <li>
+            <NavLink
+              className="nav__film nav__link_type_main"
+              activeClassName="nav__link_type_active"
+              exact
+              to="/"
+            >
+              Главная
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className="nav__film"
+              activeClassName="nav__link_type_active"
+              to="/movies"
+            >
+              Фильмы
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className="nav__film"
+              activeClassName="nav__link_type_active"
+              to="/saved-movies"
+            >
+              Сохранённые фильмы
+            </NavLink>
+          </li>
+        </ul>
+      </Route>
       <ul className="nav__list">
-        <li>
-          <Link className="nav__link nav__link_type_land" to="/sign-up">Регистрация</Link>
-        </li>
-        <li>
-          <Link className="nav__link nav__link_type_land nav__link_type_login" to="/sign-in">Войти</Link>
-        </li>
+        <Route exact path="/">
+          <li>
+            <Link className="nav__link nav__link_type_land" to="/sign-up">Регистрация</Link>
+          </li>
+          <li>
+            <Link className="nav__link nav__link_type_land nav__link_type_login" to="/sign-in">Войти</Link>
+          </li>
+        </Route>
+        <Route exact path={userRoutes}>
+          <li>
+            <NavLink
+              className="nav__link nav__link_type_profile"
+              activeClassName="nav__link_type_active"
+              to="/profile"
+            >
+              Аккаунт
+            </NavLink>
+          </li>
+        </Route>
       </ul>
     </nav>
-  </Route>
- )
+
+  );
 }
 
 export default Navigator;
