c20f9cb4dc7aab2a300b185fca0a624fc14570c1
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-09-25 14:18:14 +0300

    feat: add burger menu

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,44 @@
-// import React from 'react';
-import { Route } from "react-router-dom";
+import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 
-const headerRoutes = ['/movies', '/saved-movies', '/profile', '/'];
-// const footerRoutes = ['/movies', '/saved-movies', '/profile', '/'];
 function App() {
+  const [burgerOpen, setBurgerOpen] = React.useState(false);
+  const headRoutes = ['/movies', '/saved-movies', '/profile', '/']; // роуты где отбражется хэдер
+  const footRoutes = ['/movies', '/saved-movies', '/']; // роуты где отбражется футер
+  const openBurger = () => {
+    setBurgerOpen(true);
+  };
+  const closeBurger = () => {
+    setBurgerOpen(false);
+  };
+  React.useEffect(() => {
+    const closeByEvent = (evt) => {
+      if (evt.key === 'Escape') {
+        setBurgerOpen(false);
+      }
+      if (evt.target.classList.contains('nav_type_logged:before')) {
+        setBurgerOpen(false);
+      }
+    };
+    document.addEventListener('click', closeByEvent);
+    return () => document.removeEventListener('click', closeByEvent);
+  }, []);
   return (
     <>
-      <Route exact path={headerRoutes}>
-        <Header />
-
+      <Route exact path={headRoutes}>
+        <Header onOpen={openBurger} onClose={closeBurger} burgerOpen={burgerOpen} />
       </Route>
+      <Route exact path="/">
         <Main />
-      <Footer />
+      </Route>
+      <Route path="/movies" />
+      <Route exact path={footRoutes}>
+        <Footer />
+      </Route>
 
     </>
   );
