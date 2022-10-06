e41b9382fd50482b01e1b752930292c601f2965d
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-10-06 03:21:32 +0300

    del: delete moviesData

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,7 +6,6 @@ import './App.css';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
-import moviesData from '../../utils/movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Login from '../Login/Login';
