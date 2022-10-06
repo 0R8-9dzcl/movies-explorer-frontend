1aad247aa3743b7140f88d78468850e431fbe5e3
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-10-06 03:18:08 +0300

    fix: rename Api

--- src/utils/MoviesApi.js
+++ src/utils/MoviesApi.js
@@ -1,6 +1,6 @@
 const MOVIES_URL = 'https://api.nomoreparties.co/';
 
-function getAuthorization() {
+function getMovies() {
   return fetch(MOVIES_URL, { method: 'GET' }).then((res) => {
     if (res.ok) {
       return res.json();
@@ -8,4 +8,4 @@ function getAuthorization() {
     return Promise.reject(res.status);
   });
 }
-export default getAuthorization;
+export default getMovies;
