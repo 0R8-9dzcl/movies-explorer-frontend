a00ad3b9a69eb60251763ea4c9d4eafaf9debdb4
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2022-10-12 15:30:32 +0300

    fix: move chexkbox in form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -6,11 +6,11 @@ function SearchForm() {
       <form className="search__form" name="search">
         <input placeholder="Фильм" name="movie" type="text" className="search__input" required />
         <button type="submit" name="submit" className="button search__button">Поиск</button>
+        <label className="search__label" htmlFor="short">
+          <input className="search__short-checkbox" type="checkbox" name="short" id="short" />
+          <span className="search__short-span">Короткометражки</span>
+        </label>
       </form>
-      <label className="search__label" htmlFor="short">
-        <input className="search__short-checkbox" type="checkbox" name="short" id="short" />
-        <span className="search__short-span">Короткометражки</span>
-      </label>
     </section>
   );
 }
